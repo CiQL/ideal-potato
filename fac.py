1bbd1f39841114fe611bbeb89ab53037c4b8905f
CiQL <vn@ciql.net>
2019-05-12 10:45:50 -1000

    Edit fac and fac_test

--- fac.py
+++ fac.py
@@ -3,3 +3,7 @@
 
 def factorial(n: int) -> int:
     return 1 if n < 1 else n * factorial(n-1)
+
+if __name__ == '__main__':
+    import sys
+    print(factorial(int(sys.argv[-1])))
--- fac_test.py
+++ fac_test.py
@@ -8,6 +8,9 @@ class Factorial(unittest.TestCase):
         self.assertEqual(fac(0), 1)
     def test_positive(self):
         self.assertEqual(fac(5), 120)
+    def test_hundred(self):
+        self.assertEqual(fac(100), 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000)
+
 
 if __name__ == "__main__":
     unittest.main()
